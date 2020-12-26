b0fe2c6fc06b7b295842f0066ef945d18e3632a5
tangyuan <tangyuanbaby2016@sina.com>
2020-12-26 17:41:57 +0800

    2020.09.26_homework_interface_test_po

---
+++ interface_po_test/__init__.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time : 2020/12/26 11:46
+# @Site : 
+# @File : __init__.py.py
+# @Software: PyCharm
---
+++ interface_po_test/api_page/__init__.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time : 2020/12/26 11:51
+# @Site : 
+# @File : __init__.py.py
+# @Software: PyCharm
